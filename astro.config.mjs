fed53ec770d92639d5d5272cf788a2c25940fe97
Arge2004 <argenismedina0424@gmail.com>
2025-11-14 15:35:49 -0500

    Revert "[FEAT] Implement API routes for URL shortening and retrieval with error handling"
    This reverts commit ad51c5e2ecffd148ad4d149de3fdd22cd1c591b4.

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,16 +8,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()],
-    server: {
-      proxy: {
-        '/api': {
-          target: 'http://13.58.106.198',
-          changeOrigin: true,
-          secure: false,
-        }
-      }
-    }
+    plugins: [tailwindcss()]
   },
   site: 'https://Arge2004.github.io',
   base: '/url-shortener-frontend/',
